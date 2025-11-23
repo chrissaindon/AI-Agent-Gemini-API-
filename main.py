1b2f18145e8f836ef551595541a0054e300f95c0
Chris <chrissaindon@outlook.com>
2025-11-23 14:53:04 -0800

    Added optional --verbose arg

--- main.py
+++ main.py
@@ -5,10 +5,13 @@ from google.genai import types
 import sys
 
 
+
 def main():
     
     load_dotenv()
     
+
+    #Prompt Structure & sys.exit handling
     args = sys.argv[1:]
     if not args:
         print("Error: no prompt provided")
@@ -19,7 +22,13 @@ def main():
     messages = [
         types.Content(role="user", parts=[types.Part(text=user_prompt)])
     ]
+    
+    #Flag Structure
+    verbose = "--verbose" in sys.argv
 
+
+    
+    
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
@@ -29,10 +38,14 @@ def main():
     )
 
 
-
-    print(f"Prompt tokens:", response.usage_metadata.prompt_token_count) 
-    print("Response tokens:", response.usage_metadata.candidates_token_count)
-    print("Response:")
-    print(response.text)
+    if verbose:
+        print(f"User prompt: {user_prompt}")
+        print("Prompt tokens:", response.usage_metadata.prompt_token_count) 
+        print("Response tokens:", response.usage_metadata.candidates_token_count)
+        print("Response:")
+        print(response.text)
+    else: 
+        print("Response:")
+        print(response.text)
 
 if __name__ == "__main__": main()
